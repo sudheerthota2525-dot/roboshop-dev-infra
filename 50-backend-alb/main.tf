7fff959b4cfa5bc227d8b11bc61c74cdf4b0ebb6
Thota Shanmukha Sudheer <sudheerthota2525@gmail.com>
2025-10-31 10:20:15 -0400

    50-backend-alb

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -17,7 +17,7 @@ resource "aws_lb" "backend_alb" {
 }
 
 # Backend ALB listening on port number 80
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "backend_alb" {
   load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
@@ -30,5 +30,18 @@ resource "aws_lb_listener" "front_end" {
       message_body = "Hi, I am from backend ALB HTTP"
       status_code  = "200"
     }
+  }
+}
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    # These are ALB details, not our domain details
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
   }
 }
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z01661093A7B6GVGYSJ68"
+}
+
+variable "domain_name" {
+    default = "daws86s.fun"
+}
