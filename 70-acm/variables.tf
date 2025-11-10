642267036efb175fe01e9505f8dc95dc4cc5486b
Thota Shanmukha Sudheer <sudheerthota2525@gmail.com>
2025-11-10 13:45:05 -0500

    70-acm

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "0gworld.shop"
+}
+
+variable "zone_id" {
+    default = "Z01661093A7B6GVGYSJ68"
+}
