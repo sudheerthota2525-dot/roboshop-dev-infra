6bda224e09642c1f6adb252b0174d67dc8aea14d
Thota Shanmukha Sudheer <sudheerthota2525@gmail.com>
2025-11-14 09:46:38 -0500

    updates the sg-rules

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -327,6 +327,33 @@ resource "aws_security_group_rule" "open_vpn_1194" {
   to_port           = 1194
 }
 
+# resource "aws_security_group_rule" "catalogue_vpn" {
+#   type              = "ingress"
+#   security_group_id = local.catalogue_sg_id
+#   source_security_group_id = local.open_vpn_sg_id
+#   from_port         = 22
+#   protocol          = "tcp"
+#   to_port           = 22
+# }
+
+# resource "aws_security_group_rule" "catalogue_vpn_8080" {
+#   type              = "ingress"
+#   security_group_id = local.catalogue_sg_id
+#   source_security_group_id = local.open_vpn_sg_id
+#   from_port         = 8080
+#   protocol          = "tcp"
+#   to_port           = 8080
+# }
+
+resource "aws_security_group_rule" "components_vpn" {
+  for_each = local.vpn_ingress_rules
+  type              = "ingress"
+  security_group_id = each.value.sg_id
+  source_security_group_id = local.open_vpn_sg_id
+  from_port         = each.value.port
+  protocol          = "tcp"
+  to_port           = each.value.port
+}
 
 #This is the mistake we did, cart can't access components directly from one component to another component. they should be communicated through backend ALB
 /* resource "aws_security_group_rule" "cart_shipping" {
