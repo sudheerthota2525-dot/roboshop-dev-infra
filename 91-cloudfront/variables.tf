069b6f1a0370a9b12576ea323b2b9c9ac6edf7de
Thota Shanmukha Sudheer <sudheerthota2525@gmail.com>
2025-11-14 09:56:53 -0500

    91-cloudfront

---
+++ 91-cloudfront/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z01661093A7B6GVGYSJ68"
+}
+
+variable "domain_name" {
+    default = "0gworld.shop"
+}
